93a3f680e582bf34ce978f47999366922b98e0c5
JTiscodin <you@example.com>
2024-02-09 23:37:32 +0530

    removing mongodb credentials

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://Jatin:ujyZumigxot7VWsJ@cluster0.j3ba6jl.mongodb.net/mern?retryWrites=true&w=majority"
+      
     );
     console.log("Connected to Database")
   } catch (err) {
